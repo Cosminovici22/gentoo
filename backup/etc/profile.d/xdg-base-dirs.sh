0a56d3325fa99870e28efaaf2559861f87572bda
Cosmin Iliescu <iliescupetricacosmin@gmail.com>
2024-09-28 00:21:59 +0300

    xdg base dirs 2

--- backup/etc/profile.d/xdg-base-dirs.sh
+++ backup/etc/profile.d/xdg-base-dirs.sh
@@ -1,5 +1,5 @@
 export \
-	XDG_DATA_HOME="/home/default/.local/share" \
-	XDG_CONFIG_HOME="/home/default/.config" \
+	XDG_CONFIG_HOME="$HOME/.config" \
 	XDG_CACHE_HOME="/tmp/.cache/$LOGNAME" \
-	XDG_STATE_HOME="/home/default/.local/state"
+	XDG_DATA_HOME="$HOME/.local/share" \
+	XDG_STATE_HOME="$HOME/.local/state"
